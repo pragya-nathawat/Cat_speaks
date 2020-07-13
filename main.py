447c7d89c2848b4fea6ac16404d110923fcc4835
Pragya Nathawat <pragyanathawat@gmail.com>
2020-07-13 00:18:19 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+say = input("What's on your mind?")
+x = len(say)
+#print(x)
+print ("          "+ x*'-'+ "\n         <"+ say+">"+ "\n          "+ x*'-'+ "\n         /"+"\n /\_/\  /"+"\n( O.O )"+"\n > ^ <" )
